e7146f22d7bb8b527a2f3ebc092e599fff39e8e9
unknown <tud14560@TUK-CLATEST.tu.temple.edu>
2015-02-18 12:35:17 -0500

    Modify Assets

--- asset.aspx.designer.cs
+++ asset.aspx.designer.cs
@@ -256,6 +256,33 @@ namespace CD6 {
         protected global::System.Web.UI.WebControls.TextBox txtNotes;
         
         /// <summary>
+        /// Div1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Div1;
+        
+        /// <summary>
+        /// btnSubmitModifyAsset control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmitModifyAsset;
+        
+        /// <summary>
+        /// lblAssetID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAssetID;
+        
+        /// <summary>
         /// submit_button control.
         /// </summary>
         /// <remarks>
