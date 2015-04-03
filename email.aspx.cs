65628f4b64bf27eefe37ccfc92316cdf8732d43e
unknown <tud45086@TUK-CLATEST.tu.temple.edu>
2015-04-03 17:04:04 -0400

    Grabbing the EmailBody on the pageload, and Updating it when hitting the submit button

--- email.aspx.cs
+++ email.aspx.cs
@@ -6,9 +6,36 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Net.Mail;
 using Tools;
+using System.Data;
 namespace CD6 {
     public partial class email : System.Web.UI.Page {
+        Email myEmail = new Email();
+        DataSet myDS = new DataSet();
+        bool buttonIsClicked=false;
+
         protected void Page_Load(object sender, EventArgs e) {
+
+            if (Session["IsClicked"] == null)
+            {
+                Session.Add("IsClicked", buttonIsClicked);
+            }
+            if (Session["IsClicked"]!=null)
+            {
+                if (!IsPostBack)
+                {
+                    myDS = myEmail.GetEmailReciept();
+                    string body = myDS.Tables[0].Rows[0][0].ToString();
+                    txtEmail.Text = body;
+                    buttonIsClicked = true;
+                    Session["IsClicked"] = buttonIsClicked;
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string body = txtEmail.Text;
+            myEmail.UpdateEmailBody(body);
         }
     }
 }
