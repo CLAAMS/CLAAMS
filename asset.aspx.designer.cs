1ee3f8f39b5464530a0b27828567563e8d000c56
tbell83 <tbell@tombellnj.com>
2015-04-14 16:32:10 -0400

    changing menu orders

--- asset.aspx.designer.cs
+++ asset.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace CD6 {
     public partial class asset {
         
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
         /// lblModal_header control.
         /// </summary>
         /// <remarks>
